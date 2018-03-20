1681f6edf54b5402d36728b2c755a4226dc296a1
gz-c <token@protonmail.com>
2018-03-21 00:53:39 +0800

    Fix c2cx.Order MarshalJSON

---
+++ exchange/c2cx/c2cx_test.go
@@ -0,0 +1,60 @@
+package c2cx
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnixMilli(t *testing.T) {
+	var x int64 = 1521563904999
+	y := fromUnixMilli(x)
+	z := toUnixMilli(y)
+	require.Equal(t, x, z)
+}
+
+func TestOrderJSON(t *testing.T) {
+	var x int64 = 1521563904999
+	y := fromUnixMilli(x)
+
+	o := Order{
+		Amount:          decimal.New(123, -3),
+		AvgPrice:        decimal.New(345, -1),
+		CompletedAmount: decimal.New(321, -2),
+		Fee:             decimal.New(1, 0),
+		CreateDate:      y,
+		CompleteDate:    y,
+		OrderID:         1234,
+		Price:           decimal.New(456, -4),
+		Status:          StatusActive,
+		Type:            OrderTypeBuy,
+		Trigger:         nil,
+		CustomerID:      nil,
+		Source:          "api",
+	}
+
+	p, err := json.Marshal(o)
+	require.NoError(t, err)
+
+	t.Log(string(p))
+
+	var q Order
+	err = json.Unmarshal(p, &q)
+	require.NoError(t, err)
+	require.Equal(t, o, q)
+
+	trigger := decimal.New(789, 0)
+	o.Trigger = &trigger
+	customerID := "foo-cid"
+	o.CustomerID = &customerID
+
+	p, err = json.Marshal(o)
+	require.NoError(t, err)
+
+	q = Order{}
+	err = json.Unmarshal(p, &q)
+	require.NoError(t, err)
+	require.Equal(t, o, q)
+}
